e927a7b11da68e426c10ac81396a464ec05c6f8b
NatZuKix <nutthanon.natzuki@mail.kmutt.ac.th>
2022-09-04 04:40:01 +0700

    1stsent

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,15 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(word==null||word==undefined){
+    return undefined;
+  }else if(word.startsWith(startWord)){
+    return word;
+  }else{
+    return startWord+word;
+  }
 }
-module.exports = fillStartWord
+//module.exports = fillStartWord
+console.log(fillStartWord("js","beginner"));
+console.log(fillStartWord("first","firststep"));
+console.log(fillStartWord("first",null));
